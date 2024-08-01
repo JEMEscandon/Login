9fc8725af809a1a393a11b2f1689e71dc864824c
Nelson Ysain Ortiz Leal <nelsonortizleal@gmail.com>
2024-08-01 00:12:36 -0600

    Update: Iniciando mi apliación con FastAPI y 2 endpoints get

--- Algoritmo/app.py
+++ Algoritmo/app.py
@@ -1,1 +1,25 @@
-print("Primeras lineas para crear el super algoritmo")
+# Importando FastAPI
+from fastapi import FastAPI
+
+# Creando una BD de forma temporal
+posts = []
+
+# Creando la aplicacion
+app = FastAPI()
+
+# EndPoint para la ruta raiz
+@app.get('/')
+def read_root():
+    return {"Bienvenido": "Bienvenido a mi API REST con FastAPI"}
+
+@app.get('/posts')
+def get_post():
+    return posts
+
+# --------------------------------------------------------------
+# Para Ejecutar la aplicación, ejecutar el siguiente comando:
+# uvicorn app:app
+# app -> Referencia al archivo y app -> Referencia a la variable del servidor
+
+# Para ejecutarlo de forma automatico
+# uvicorn app:app --reload
